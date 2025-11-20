47f51cd2271d24ce719d59d6ccff3cec9d311231
kjyang <kjyang@utarex.com>
2025-11-20 14:59:16 +0900

    Updated kotlin core version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.utarex.inoutbarsampleapp"
-    compileSdk = 36
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.utarex.inoutbarsampleapp"
         minSdk = 31
-        targetSdk = 36
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
